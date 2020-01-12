c9f124f8ae049ce6a4716b82c3dbfaf7006eb711
swati2788 <parulekar.swati@gmail.com>
2020-01-12 12:56:57 +0530

    this is my first project commit

---
+++ src/main/java/Hello/hello1.java
@@ -0,0 +1,15 @@
+package Hello;
+
+public class hello1 {
+
+	public void paymentTransfer()
+	{
+		System.out.println("in payment");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/java/test/NewTest.java
@@ -0,0 +1,25 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import Hello.hello1;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class NewTest {
+  @Test
+  public void f() throws InterruptedException {
+	  
+	  WebDriverManager.chromedriver().setup();
+	  //hello1 h1=new hello1();
+	  //h1.paymentTransfer();
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com");
+		Thread.sleep(5000);
+		
+		driver.quit();
+
+  }
+}
